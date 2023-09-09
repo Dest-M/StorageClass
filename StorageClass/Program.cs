13a1f85cb27945ef2d177573c16fb6b78e1a8b41
Dest <morgisarts@gmail.com>
2023-09-09 16:18:29 +0300

    Add project files.

---
+++ StorageClass/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage_Class
+{
+
+    internal class Menu
+    {
+        static void Main(string[] args)
+        {
+            Product_Class[] products = new Product_Class[100];
+
+            int proCount = 0;
+            string input;
+            bool flag = false;
+            int id = 1000;
+            while (true)
+            {
+                Product_Class a = new Product_Class();
+                Console.WriteLine("Enter a command \nSyntax: \nADD(int,str,int,int) \nSELECT *: ");
+                input = Console.ReadLine();
+                if (input.Contains("ADD"))
+                {
+                    flag = a.ADD(input, id);
+                    products[proCount] = new Product_Class(a);
+                    if (flag == true)
+                    {
+                        proCount++;
+                        id++;
+
+                    }
+                }
+                if (input.Contains("SELECT *"))
+                {
+                    for (int i = 0; i < proCount; i++)
+                    {
+                        Console.WriteLine("------------------\nId: " + Convert.ToString(products[i].id) + "\nStorage Number: " + Convert.ToString(products[i].invenNum) + "\nName: " + products[i].name + "\nPrice: " + Convert.ToString(products[i].price) + "\nAmount: " + Convert.ToString(products[i].amount) + "\n-----------------------------------");
+
+                    }
+
+                }
+
+            }
+        }
+
+    }
+
+}
---
+++ StorageClass/Storage.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage_Class
+{
+    internal class Product_Class
+    {
+        public int id;
+        public int invenNum;
+        public bool inStore = false;
+        public float price;
+        public int amount;
+        public string name;
+
+        public Product_Class() { }
+        public Product_Class(Product_Class a)
+        {
+            id = a.id;
+            invenNum = a.invenNum;
+            inStore = a.inStore;
+            price = a.price;
+            amount = a.amount;
+            name = a.name;
+        }
+        Product_Class(int invenNu, string nam, float pric, int amoun)
+        {
+            invenNum = invenNu;
+            name = nam;
+            price = pric;
+            amount = amoun;
+
+
+        }
+        public bool ADD(string inp, int id)
+        {
+            int switching = 0;
+            char letter;
+            char[] input = inp.ToCharArray();
+            bool flag = false;
+
+            int storNum = 0;
+            string name = "";
+            int price = 0;
+            int amount = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i <= 3)
+                {
+                    ///Console.WriteLine("ADD:");
+
+                }
+                else if (input[i] >= '0' && input[i] <= '9' && switching == 0)
+                {
+                    storNum = storNum * 10 + Convert.ToInt32(input[i] - '0');
+                    //Console.WriteLine("Switch " + Convert.ToString( switching)+" "+ Convert.ToString(storNum));
+                    if (i == 9)
+                    {
+                        switching++;
+                        i++;
+                    }
+
+                }
+                else if (switching == 1)
+                {
+                    if (inp[i] == ',')
+                    {
+                        switching++;
+                    }
+                    else if ((input[i] >= 'a' && input[i] <= 'z') || (input[i] >= 'A' && input[i] <= 'Z'))
+                    {
+
+                        letter = (char)input[i];
+                        name = name + letter;
+                    }
+                    else
+                    {
+                        switching = 5;
+                    }
+                    //Console.WriteLine("Switch " + Convert.ToString( switching)+" "+ name);
+
+                }
+                else if (switching == 2)
+                {
+                    if (input[i] == ',')
+                    {
+                        switching++;
+
+                    }
+                    else if (input[i] >= '0' || input[i] <= '9')
+                    {
+                        price = price * 10 + Convert.ToInt32(input[i] - '0');
+                    }
+                    else
+                    {
+                        switching = 5;
+                    }
+                    //Console.WriteLine("Switch " + Convert.ToString( switching)+" "+ Convert.ToString(price));
+
+
+                }
+                else if (switching == 3)
+                {
+                    if (input[i] == ')')
+                    {
+                        flag = true;
+                        switching++;
+                    }
+                    else if (input[i] >= '0' || input[i] <= '9')
+                    {
+                        amount = amount * 10 + Convert.ToInt32(input[i] - '0');
+                    }
+                    else
+                    {
+                        switching = 5;
+                    }
+                    //Console.WriteLine("Switch " + Convert.ToString( switching)+" "+ Convert.ToString(amount));
+                }
+
+                else
+                {
+                    Console.WriteLine("Input Error, wrong line");
+                    return flag;
+
+                }
+
+            }
+            if (flag == true)
+            {
+                this.id = id;
+                this.invenNum = storNum;
+                this.name = name;
+                this.price = price;
+                this.amount = amount;
+
+
+
+            }
+            return flag;
+
+        }
+    }
+}
